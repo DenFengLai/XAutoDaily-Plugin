969930b05c6f3cc4ef4b4c06435c2e267cfdb1a0
等风来 <dengfenglai547@qq.com>
2025-02-07 16:00:37 +0800

    ✨ feat(ThumbUp): 添加好友列表选择功能并优化Cron表达式提示

--- guoba/schemas/ThumbUp.js
+++ guoba/schemas/ThumbUp.js
@@ -10,10 +10,19 @@ export default [
     component: "Switch"
   },
   {
+    field: "ThumbUp.list",
+    helpMessage: "好友列表",
+    label: "需要进行点赞的好友",
+    componentProps: {
+      placeholder: "点击选择好友"
+    },
+    component: "GSelectFriend"
+  },
+  {
     field: "ThumbUp.cron",
     label: "自动点赞定时表达式",
     helpMessage: "修改后重启生效",
-    bottomHelpMessage: "自动检查Cron表达式",
+    bottomHelpMessage: "自动点赞Cron表达式",
     component: "EasyCron",
     componentProps: {
       placeholder: "请输入Cron表达式"
